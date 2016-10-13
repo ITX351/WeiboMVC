d49e60800ea8f46c773dbb2bbb41abca06b360ca
Your Name <861265654@qq.com>
2016-10-13 22:27:14 +0800

    发布微博功能

---
+++ src/hit/weibo/action/PublishAction.java
@@ -0,0 +1,26 @@
+package hit.weibo.action;
+
+import hit.weibo.persistence.repository.WeiboRepository;
+import hit.weibo.util.LoginStatus;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
+
+public class PublishAction implements Action {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+        // insert into
+        String content = request.getParameter("content");
+
+        HttpSession session = request.getSession();
+        LoginStatus loginStatus = (LoginStatus) session.getAttribute("loginStatus");
+        if (loginStatus != null) {
+            WeiboRepository.insertWeibo(loginStatus.getUser_id(), content);
+            return "weibo.jsp";
+        }
+//        WeiboRepository.insertWeibo(1, content);
+        return "error.html";
+    }
+}
